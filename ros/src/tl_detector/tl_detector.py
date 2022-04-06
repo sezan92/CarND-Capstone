a19efcddd7c12bb3e15d0966575ba978488d2090
sezan <sezan92>
2022-04-06 22:19:58 +0900

    tl_detector code for classification

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -12,6 +12,7 @@ import cv2
 import yaml
 
 STATE_COUNT_THRESHOLD = 3
+CLASSIFY = False
 
 class TLDetector(object):
     def __init__(self):
@@ -114,14 +115,17 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+        if CLASSIFY:
+            if(not self.has_image):
+                self.prev_light_loc = None
+                return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+            cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)
+            #Get classification
+            return self.light_classifier.get_classification(cv_image)
+        else:
+            return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
